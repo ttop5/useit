5f1621b7527b219911a6c5f522538d385b2e2da8
ttop5 <ttop5@qq.com>
2017-05-03 00:12:22 +0800

    feat: 添加mock数据

---
+++ app/src/mock/db.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const faker = require('faker')
+const dateformat = require('dateformat')
+
+// 首页
+const news = []
+for (let i=0; i<20; i++) {
+  news.push({
+    id: i,
+    title: `大事不好啦！皮革厂老板跟小姨子跑啦！—— ${i}`,
+    imgUrl: `/imgs/${i}.png`,
+    content: '浙江温州，浙江温州，最大皮革厂，江南皮革厂倒闭了！王八蛋老板黄鹤吃喝嫖赌，欠下了3.5个亿，带着他的小姨子跑了。我们没有没有办法，拿着钱包抵公司。原价都是三百多、二百多、一百多的钱包，通通二十块，通通二十块！黄鹤王八蛋，你不是人，我们辛辛苦苦给你干了大半年，你不发工资，你还我血汗钱，还我血汗钱！',
+  })
+}
+
+// 广场
+const square = []
+for (let i=0; i<20; i++) {
+  square.push({
+    id: i,
+    user: `user-${i}`,
+    content: '我是个数码达人，我喜欢各种电子产品。'
+  })
+}
+
+module.exports = {
+  news,
+  square,
+}
---
+++ app/src/mock/server.js
@@ -0,0 +1,29 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const dbFile = require('./db.js');
+const router = jsonServer.router(dbFile)
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(jsonServer.rewriter({
+  '/api/': '/',
+  '/auth/login': '/login'
+}))
+
+
+server.use(jsonServer.bodyParser)
+server.post('/login', function (req, res, next) {
+  const username = req.body.username
+  if (username === 'admin') {
+    res.jsonp({ username, token: 'test_token' })
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+
+server.use(router)
+server.listen(3001, function () {
+  console.log('JSON Server is running')
+})
