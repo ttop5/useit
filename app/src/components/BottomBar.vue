f6cb786a3e31ebb0e5c62373c912a9446b74b604
ttop5 <ttop5@qq.com>
2017-05-05 00:49:03 +0800

    perf: icon本地化

--- app/src/components/BottomBar.vue
+++ app/src/components/BottomBar.vue
@@ -3,19 +3,19 @@
     <div class="bottombar-view">
       <tabbar>
         <tabbar-item link='/'>
-          <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+          <img slot="icon" src="../assets/icons/icon_nav_button.png">
           <span slot="label">首页</span>
         </tabbar-item>
         <tabbar-item link='/news'>
-          <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_msg.png">
+          <img slot="icon" src="../assets/icons/icon_nav_msg.png">
           <span slot="label">新闻</span>
         </tabbar-item>
         <tabbar-item link='/square'>
-          <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_article.png">
+          <img slot="icon" src="../assets/icons/icon_nav_article.png">
           <span slot="label">广场</span>
         </tabbar-item>
         <tabbar-item link='/me'>
-          <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_cell.png">
+          <img slot="icon" src="../assets/icons/icon_nav_cell.png">
           <span slot="label">我的</span>
         </tabbar-item>
       </tabbar>
