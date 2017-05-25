7b54cf1fbf9fd0d232bbb8e67330b29947f3f1dd
ttop5 <ttop5@qq.com>
2017-05-25 23:50:39 +0800

    refactor: 提取路由配置

--- src/main.js
+++ src/main.js
@@ -1,72 +1,12 @@
 import Vue from 'vue';
 import FastClick from 'fastclick';
 import VueRouter from 'vue-router';
+
 import App from './App';
-import Home from './views/Home';
-import Products from './views/Products';
-import News from './views/News';
-import NewsItem from './views/NewsItem';
-import Square from './views/Square';
-import Me from './views/Me';
-import MyArticle from './views/MyArticle';
-import AccountSetting from './views/AccountSetting';
-import ChangePasswd from './views/ChangePasswd';
-import AboutSoft from './views/AboutSoft';
-import Login from './views/Login';
-import Register from './views/Register';
+import routes from './router/index';
+
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    component: Home,
-  },
-  {
-    path: '/products',
-    component: Products,
-  },
-  {
-    path: '/news',
-    component: News,
-  },
-  {
-    path: '/news_item',
-    component: NewsItem,
-  },
-  {
-    path: '/square',
-    component: Square,
-  },
-  {
-    path: '/me',
-    component: Me,
-  },
-  {
-    path: '/myarticle',
-    component: MyArticle,
-  },
-  {
-    path: '/accountsetting',
-    component: AccountSetting,
-  },
-  {
-    path: '/changepasswd',
-    component: ChangePasswd,
-  },
-  {
-    path: '/aboutsoft',
-    component: AboutSoft,
-  },
-  {
-    path: '/login',
-    component: Login,
-  },
-  {
-    path: '/register',
-    component: Register,
-  },
-];
 
 const router = new VueRouter({
   routes,
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,66 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Home from '../views/Home';
+import Products from '../views/Products';
+import News from '../views/News';
+import NewsItem from '../views/NewsItem';
+import Square from '../views/Square';
+import Me from '../views/Me';
+import MyArticle from '../views/MyArticle';
+import AccountSetting from '../views/AccountSetting';
+import ChangePasswd from '../views/ChangePasswd';
+import AboutSoft from '../views/AboutSoft';
+import Login from '../views/Login';
+import Register from '../views/Register';
 
-Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
-  ],
-});
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/products',
+    component: Products,
+  },
+  {
+    path: '/news',
+    component: News,
+  },
+  {
+    path: '/news_item',
+    component: NewsItem,
+  },
+  {
+    path: '/square',
+    component: Square,
+  },
+  {
+    path: '/me',
+    component: Me,
+  },
+  {
+    path: '/myarticle',
+    component: MyArticle,
+  },
+  {
+    path: '/accountsetting',
+    component: AccountSetting,
+  },
+  {
+    path: '/changepasswd',
+    component: ChangePasswd,
+  },
+  {
+    path: '/aboutsoft',
+    component: AboutSoft,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    component: Register,
+  },
+];
+
+export default routes;
